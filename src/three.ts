a39cafed8c91a21a5d3f9a6098faed322eb1bb7c
Niek <17721120+Niekvan@users.noreply.github.com>
2020-08-20 20:11:38 +0200

    reset to basic

--- src/three.ts
+++ src/three.ts
@@ -10,12 +10,10 @@ import {
   DirectionalLight,
   Vector2,
   Renderer,
-  AnimationMixer,
 } from 'three';
 
-import Modal from './modal';
-
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const renderer = new WebGLRenderer({ antialias: true });
 const raycaster = new Raycaster();
@@ -24,47 +22,54 @@ const camera = new PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.1,
-  2000
+  1000
 );
 
-let mouse = [0.5, 0.5];
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.maxPolarAngle = Math.PI / 2;
+controls.minPolarAngle = Math.PI / 3;
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.dampingFactor = 0.1;
 
-// const hemiLight = new HemisphereLight(0xe6e210, 0xf0f0f0, 0.61);
-// const directionalLight = new DirectionalLight(0xffffff, 0.1);
+const hemiLight = new HemisphereLight(0xffffff, 0xffffff, 0.61);
+const directionalLight = new DirectionalLight(0xffffff, 0.54);
 
 export const init = () => {
-  scene.background = new Color(0x000000);
+  scene.background = new Color(0xf1f1f1);
 
-  // scene.add(hemiLight);
+  hemiLight.position.set(0, 50, 0);
+  scene.add(hemiLight);
 
-  // const d = 8.25;
-  // directionalLight.position.set(-8, 12, 2000);
-  // directionalLight.castShadow = true;
-  // directionalLight.shadow.mapSize = new Vector2(1024, 1024);
-  // directionalLight.shadow.camera.near = 0.1;
-  // directionalLight.shadow.camera.far = 3000;
-  // directionalLight.shadow.camera.left = d * -1;
-  // directionalLight.shadow.camera.right = d;
-  // directionalLight.shadow.camera.top = d;
-  // directionalLight.shadow.camera.bottom = d * -1;
+  const d = 8.25;
+  directionalLight.position.set(-8, 12, 8);
+  directionalLight.castShadow = true;
+  directionalLight.shadow.mapSize = new Vector2(1024, 1024);
+  directionalLight.shadow.camera.near = 0.1;
+  directionalLight.shadow.camera.far = 3000;
+  directionalLight.shadow.camera.left = d * -1;
+  directionalLight.shadow.camera.right = d;
+  directionalLight.shadow.camera.top = d;
+  directionalLight.shadow.camera.bottom = d * -1;
 
-  // scene.add(directionalLight);
+  scene.add(directionalLight);
 
-  camera.position.set(0, 0, 300);
+  camera.position.set(0, 0, 5);
 
   const loader = new GLTFLoader();
-  loader.load('/public/200819_ISLAND_TEST.glb', (gltf) => {
-    const model = gltf.scene;
-    model.rotation.x = Math.PI / 4;
+  loader.load(
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/chair.glb',
+    (gltf) => {
+      const model = gltf.scene;
+      model.scale.set(2, 2, 2);
+      model.position.y = -1;
+      model.rotation.y = Math.PI;
 
-    // const mixer = new AnimationMixer(model);
-    // const action = mixer.clipAction(gltf.animations[0]);
-    // action.play();
-
-    scene.add(model);
-    document.addEventListener('click', onMouseClick);
-    render();
-  });
+      scene.add(model);
+      document.addEventListener('click', onMouseClick);
+      render();
+    }
+  );
 
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -72,7 +77,6 @@ export const init = () => {
 
   document.getElementById('three').appendChild(renderer.domElement);
   window.addEventListener('resize', onWindowResize, false);
-  window.addEventListener('mousemove', onMouseMove, false);
 };
 
 const onWindowResize = () => {
@@ -99,15 +103,9 @@ const onMouseClick = (e: MouseEvent) => {
     object.traverse((o: SkinnedMesh) => {
       if (o.isMesh) {
         console.log(o.name);
-        Modal.showModal(o.name);
       }
     });
   }
-};
-
-const onMouseMove = (e: MouseEvent) => {
-  mouse[0] = e.clientX / window.innerWidth;
-  mouse[1] = e.clientY / window.innerHeight;
 };
 
 function resizeRendererToDisplaySize(renderer: Renderer) {
@@ -125,16 +123,13 @@ function resizeRendererToDisplaySize(renderer: Renderer) {
 }
 
 export function render() {
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+
   if (resizeRendererToDisplaySize(renderer)) {
     const canvas = renderer.domElement;
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
   }
-
-  camera.position.x = Math.sin(0.5 * Math.PI * (mouse[0] - 0.5)) * 300;
-  camera.position.y = Math.sin(0.25 * Math.PI * (mouse[1] - 0.5)) * 300;
-  camera.lookAt(scene.position);
-
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
 }
